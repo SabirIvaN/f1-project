203e4087ea141ce4af795e59ca7d660594177b06
Иван Сабиров <sabirivan@yandex.com>
2021-08-26 14:26:46 +0300

    Rollbar is installed and minor syntax corrections are made

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    Log::debug('Here is some debug information');
 });
