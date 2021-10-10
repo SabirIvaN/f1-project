962b6fb9fa247c9f0ca5be2525b9e376ec88fd53
Иван Сабиров <sabirivan@yandex.com>
2021-10-10 17:41:54 +0300

    Fixes added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,20 +15,19 @@ class DatabaseSeeder extends Seeder
     {
         /* Factories */
 
-        \App\Models\Position::factory()->count(2)->create();
-        \App\Models\Service::factory()->count(3)->create();
-        \App\Models\City::factory()->count(5)->create();
-        \App\Models\User::factory()->count(2)
-            ->for(\App\Models\Position::all()->random())
-            ->create();
-        \App\Models\Order::factory()->count(5)
-            ->hasAttached(\App\Models\City::all()->random())
-            ->hasAttached(\App\Models\Service::all()->random())
-            ->create();
+        // \App\Models\Position::factory()->count(2)->create();
+        // \App\Models\Service::factory()->count(3)->create();
+        // \App\Models\City::factory()->count(5)->create();
+        // \App\Models\User::factory()->count(2)
+        //     ->for(\App\Models\Position::all()->random())
+        //     ->create();
+        // \App\Models\Order::factory()->count(5)
+        //     ->hasAttached(\App\Models\City::all()->random())
+        //     ->hasAttached(\App\Models\Service::all()->random())
+        //     ->create();
 
         /* Seeders */
 
-        /*
         $this->call([
             PositionSeeder::class,
             ServiceSeeder::class,
@@ -37,6 +36,5 @@ class DatabaseSeeder extends Seeder
             OrderSeeder::class,
             OrderableSeeder::class,
         ]);
-        */
     }
 }
