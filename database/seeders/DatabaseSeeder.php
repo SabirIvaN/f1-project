3d1dfb7b0c981df5e27ff34ef8d94bcc990fa66c
Иван Сабиров <sabirivan@yandex.com>
2022-02-07 16:39:17 +0300

    Improvements added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -33,8 +33,8 @@ class DatabaseSeeder extends Seeder
             ServiceSeeder::class,
             CitySeeder::class,
             UserSeeder::class,
-            OrderSeeder::class,
-            OrderableSeeder::class,
+            // OrderSeeder::class,
+            // OrderableSeeder::class,
         ]);
     }
 }
